2f4bbd4f2a0b5d97b7a315e01c41c79b153b62bc
lin.lo@husky.neu.edu <lin.lo@husky.neu.edu>
2022-05-23 21:33:52 -0400

    add quiz 1 part 2

--- quiz/main.go
+++ quiz/main.go
@@ -5,10 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
-	csvFilename := flag.String("csv", "problem.csv", "a csv file in the format of 'questions,answers")
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'questions,answers")
+	timeLimit := flag.Int("limit", 30, "time limit in seconds")
+
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -27,19 +30,30 @@ func main() {
 
 	problems := linesToProblems(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	numCorrect := 0
 
 	for i, problem := range problems {
 		fmt.Printf("Problem %d: %s = \n", i+1, problem.question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
 
-		if answer == problem.answer {
-			numCorrect++
+		answerChannel := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerChannel <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("You got %d out of %d problems correct!", numCorrect, len(problems))
+			return
+		case answer := <-answerChannel:
+			if answer == problem.answer {
+				numCorrect++
+			}
 		}
 	}
-
-	fmt.Printf("You got %d out of %d problems correct!", numCorrect, len(problems))
 }
 
 func linesToProblems(lines [][]string) []problem {
